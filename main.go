06d1627722c9cfabde3ed9cf35734f0580f49fdf
SocialToad <udzejson@gmail.com>
2022-10-22 01:41:15 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/test1", server) // заметка.тест
+	http.HandleFunc("/", server1)     // заметка.текст
+	http.ListenAndServe(":8888", nil)
+}
+func server(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "узбеки")
+}
+func server1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "goblin")
+}
